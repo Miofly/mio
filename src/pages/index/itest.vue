c40e2c2b20879b096eff168f95adeff31b6fafd6
miofly <953212389@qq.com>
2020-04-23 11:00:41 +0800

    update

--- src/pages/index/itest.vue
+++ src/pages/index/itest.vue
@@ -2,9 +2,9 @@
 	<div class="flex-box-column-center pdf">
 		<p class="margin-top bg-red" ref="copyBtn"
 		   @click="copy($event)"
-		   @touchstart="copy($event)"
 		>复制发票地址</p>
 	</div>
+	<!--@touchstart="copy($event)"-->
 </template>
 <script>
     import Clipboard from 'clipboard'
@@ -17,24 +17,23 @@
             return {
                 pdfUrl: '213321123213', // pdf的网址
                 clipboard: null, // clipboard的实例
-                clipboard1: null // clipboard的实例
+
             }
         },
         mounted() {
-            console.log(231)
             this.clipboard = new Clipboard(this.$refs.copyBtn, {text: () => this.pdfUrl})
         },
         methods: {
             async copy(e, text) {
                 const data = await commonGet('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxa4cec9585a0c00d6&secret=4e3a25657f654f522a820189dc41138a')
-                this.pdfUrl = data.errcode
+                this.pdfUrl = data.expires_in
                 this.clipboard.on('success', e => {
                     this.ui.showToast('复制成功')
                 })
                 this.clipboard.on('error', e => {
                     this.ui.showToast('213312')
                 })
-            },
+            }
         }
     }
 </script>

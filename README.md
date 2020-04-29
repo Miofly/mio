db84d36d1fdff6208ac86fe3a9e5aea2dca329be
miofly <953212389@qq.com>
2020-04-29 15:12:02 +0800

    更新

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # mio
 uniapp 项目模块 --- 完全 vue 模式开发
 
-# 安装项目
+## 安装项目
 npm install --registry=https://registry.npm.taobao.org
 建议运行npm，因为cnpm会出现一些未知问题
 
-如果一直有问题可直接拷贝项目的node_modules：下方为百度云链接
-链接：https://pan.baidu.com/s/1DUVkStXfh-8fx2dPWNJ2jw 
+如果一直有问题可直接拷贝项目的node_modules：
+
+### 下方为百度云链接
+链接：https://pan.baidu.com/s/1DUVkStXfh-8fx2dPWNJ2jw <br>
 提取码：aqh0
 
-使用 npm start 启动项目即可
+### 启动项目
+npm start
 
 功能模块都在 components 文件夹内
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,50 +1,51 @@
 <template>
-    <view style="height: 100%">
-        <setting v-if="PageCur==='setting'"></setting>
-        <user v-if="PageCur==='user'"></user>
-        <index v-if="PageCur==='index'"></index>
-        <module v-if="PageCur==='module'"></module>
-        <plugin v-if="PageCur==='plugin'"></plugin>
+	<view style="height: 100%">
+		<setting v-if="PageCur==='setting'"></setting>
+		<user v-if="PageCur==='user'"></user>
+		<index v-if="PageCur==='index'"></index>
+		<module v-if="PageCur==='module'"></module>
+		<plugin v-if="PageCur==='plugin'"></plugin>
 
-        <view class="cu-bar tabbar bg-white foot">
-            <view @click="NavChange" class="action" data-cur="setting">
-                <view class="fa-cu-image">
-                    <image :src="'/static/tabbar/setting' + [PageCur=='setting'?'_blue_cur':''] + '.png'"></image>
-                </view>
-                <view :class="PageCur=='setting'?'text-blue':'text-gray'">{{indexData[1]}}</view>
-            </view>
-            <view @click="NavChange" class="action" data-cur="module">
-                <view class="fa-cu-image">
-                    <image :src="'/static/tabbar/module' + [PageCur=='module'?'_blue_cur':''] + '.png'"></image>
-                </view>
-                <view :class="PageCur=='module'?'text-blue':'text-gray'">{{indexData[2]}}</view>
-            </view>
-            <view @click="NavChange" class="action text-gray add-action" data-cur="index">
-                <button class="cu-btn fa-plus fa bg-blue shadow"></button>
-                <view :class="PageCur=='index'?'text-blue':'text-gray'">{{indexData[0]}}</view>
-            </view>
-            <view @click="NavChange" class="action" data-cur="plugin">
-                <view class="fa-cu-image">
-                    <image :src="'/static/tabbar/plugin' + [PageCur=='plugin'?'_blue_cur':''] + '.png'"></image>
-                    <view class="cu-tag badge">99</view>
-                </view>
-                <view :class="PageCur=='plugin'?'text-blue':'text-gray'">{{indexData[3]}}</view>
-            </view>
-            <view @click="NavChange" class="action" data-cur="user">
-                <view class="fa-cu-image">
-                    <image :src="'/static/tabbar/user' + [PageCur=='user'?'_blue_cur':''] + '.png'"></image>
-                    <view class="cu-tag badge">99</view>
-                </view>
-                <view :class="PageCur=='user'?'text-blue':'text-gray'">{{indexData[4]}}</view>
-            </view>
-        </view>
-    </view>
+		<view class="cu-bar tabbar bg-white foot">
+			<view @click="NavChange" class="action" data-cur="setting">
+				<view class="fa-cu-image">
+					<image :src="'/static/tabbar/setting' + [PageCur=='setting'?'_blue_cur':''] + '.png'"></image>
+				</view>
+				<view :class="PageCur=='setting'?'text-blue':'text-gray'">{{indexData[1]}}</view>
+			</view>
+			<view @click="NavChange" class="action" data-cur="module">
+				<view class="fa-cu-image">
+					<image :src="'/static/tabbar/module' + [PageCur=='module'?'_blue_cur':''] + '.png'"></image>
+				</view>
+				<view :class="PageCur=='module'?'text-blue':'text-gray'">{{indexData[2]}}</view>
+			</view>
+			<view @click="NavChange" class="action text-gray add-action" data-cur="index">
+				<button class="cu-btn fa-plus fa bg-blue shadow"></button>
+				<view :class="PageCur=='index'?'text-blue':'text-gray'">{{indexData[0]}}</view>
+			</view>
+			<view @click="NavChange" class="action" data-cur="plugin">
+				<view class="fa-cu-image">
+					<image :src="'/static/tabbar/plugin' + [PageCur=='plugin'?'_blue_cur':''] + '.png'"></image>
+					<view class="cu-tag badge">99</view>
+				</view>
+				<view :class="PageCur=='plugin'?'text-blue':'text-gray'">{{indexData[3]}}</view>
+			</view>
+			<view @click="NavChange" class="action" data-cur="user">
+				<view class="fa-cu-image">
+					<image :src="'/static/tabbar/user' + [PageCur=='user'?'_blue_cur':''] + '.png'"></image>
+					<view class="cu-tag badge">99</view>
+				</view>
+				<view :class="PageCur=='user'?'text-blue':'text-gray'">{{indexData[4]}}</view>
+			</view>
+		</view>
+	</view>
 </template>
 
 <script>
     import {mapState} from 'vuex'
+
     export default {
-        data () {
+        data() {
             return {
                 // #ifdef MP-WEIXIN
                 PageCur: this.$store.state.indexPage,
@@ -54,7 +55,7 @@
                 // #endif
             }
         },
-        onLoad () {
+        onLoad() {
             console.log(this.$store.state.indexPage)
             console.log('onLoad：页面加载完成')
 
@@ -66,26 +67,26 @@
                 }
             }, 1000)
         },
-        onUnload () {
+        onUnload() {
             console.log('onUnload：页面卸载')
         },
-        onShow () {
+        onShow() {
             console.log('onShow：页面显示')
         },
-        onHide () {
+        onHide() {
             console.log('onHide：页面隐藏')
         },
-        onReady () {
+        onReady() {
             console.log('onReady：页面初次渲染完成')
         },
-        onResize () {
+        onResize() {
             console.log('onResize：页面尺寸发生变化')
         },
-        onBackPress () {
+        onBackPress() {
             console.log('onBackPress：页面返回')
         },
         methods: {
-            NavChange (e) {
+            NavChange(e) {
                 this.PageCur = e.currentTarget.dataset.cur
             }
         },

fe2d0dcad89070f46dd665fea65ed5c819da672c
miofly <953212389@qq.com>
2020-07-09 21:02:46 +0800

    更新

--- src/pages/index/itest.vue
+++ src/pages/index/itest.vue
@@ -5,39 +5,18 @@
 </template>
 
 <script>
+    import {
+        publicGet
+    } from '@/api'
+
     export default {
-        data() {
-            return {
-                list: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-                toView: ''
-            }
+        async onLoad() {
+            const data = await publicGet('https://api.weixin.qq.com/cgi-bin/shorturl?access_token=ACCESS_TOKEN')
+            // console.log(data)
         },
-        methods: {
-            bindToView(event) {
-                var id = event.currentTarget.dataset.id
-                console.log(id)
-                this.toView = id
-            }
-        }
+        data() {
+
+        },
+        methods: {}
     }
 </script>
-
-<style lang="scss">
-	.container{
-		width: 100%;
-		display: flex;
-		scroll-view{
-			height: 100vh;
-			.left{
-				font-size: 50rpx;
-				line-height: 150rpx;
-			}
-		}
-		> view{
-			position: fixed;
-			right: 10rpx;
-		}
-
-	}
-</style>
-
